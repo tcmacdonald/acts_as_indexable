48444e47d500531512363e54652ccc441d556bfa
Gabriel Smith <gabriel.smith01@gmail.com>
2016-04-11 08:51:46 -0400

    some javascript started leaking through

--- app/helpers/acts_as_indexable/acts_as_indexable_helper.rb
+++ app/helpers/acts_as_indexable/acts_as_indexable_helper.rb
@@ -8,7 +8,7 @@ module ActsAsIndexable
              locals: { opts: opts }
     end
 
-    def sortable(column, title=null)
+    def sortable(column, title=nil)
       title ||= column.titleize
       css_class = column == sort_column ? "current #{sort_direction}" : nil
       direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
